9a12cd844dc6ca3ca3b05bf59733749b0c1f8c45
fuweihan <fuweihan@58.com>
2020-07-31 16:38:25 +0800

    6

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let b = 'd';
 let dd = 'df';
+<<<<<<< HEAD
 let df = 'fgfg';
 let dfg= 'fgdfg';
 let dfgd= 'fgdfddg';
+=======
+let df = 'fgfg';
+>>>>>>> parent of e74b30b... 3
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let a = 0;
+<<<<<<< HEAD
 let b = 4554;
 let c = 4545;
+=======
+let v = 44;
+>>>>>>> parent of e74b30b... 3
